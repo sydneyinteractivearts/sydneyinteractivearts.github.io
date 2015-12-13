c9d602452d5eb3a287e7a93b21912cb166779b31
Mark C Mitchell <mark@markcmitchell.net>
2015-12-13 17:28:27 +1100

    [gulpfile]: Add gulp-rename to package.json; rename _scripts/main.js to scripts/main-compiled.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var browserSync = require('browser-sync');
 var shell = require('gulp-shell');
-
+var rename = require('gulp-rename');
 
 gulp.task('transpile', function() {
     return gulp.src('_scripts/main.js')
     .pipe(babel({
         presets: ['es2015']
     }))
+    .pipe(rename('main-compiled.js'))
     .pipe(gulp.dest('scripts/'))
 });
 

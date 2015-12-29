9690e259e7b0d24bb3f252a7e9cb8efc8d17864b
Mark C Mitchell <mark@markcmitchell.net>
2015-12-29 16:44:38 +1100

    Let's just forget about Jekyll altogether, ok?

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,26 @@ var shell = require('gulp-shell');
 var rename = require('gulp-rename');
 
 gulp.task('transpile', function() {
-    return gulp.src('_assets/scripts/main.js')
+    return gulp.src('js/main.js')
     .pipe(babel({
         presets: ['es2015']
     }))
     .pipe(rename('main.compiled.js'))
-    .pipe(gulp.dest('_assets/scripts/'))
+    .pipe(gulp.dest('js/'))
 });
-
-gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
 
 gulp.task('livereload', browserSync.reload);
 
 gulp.task('serve', function() {
     browserSync.init({
         server: {
-            baseDir: '_site/'
+            baseDir: './'
         },
         open: false
     });
     
-    gulp.watch('_assets/scripts/main.js', ['transpile', 'livereload']);
+    gulp.watch(['js/main.js', 'index.html'], ['transpile', 'livereload']);
 });
 
 
-gulp.task('default', ['transpile', 'build', 'serve']);
+gulp.task('default', ['transpile', 'serve']);
